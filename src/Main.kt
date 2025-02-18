ec7a423b942342676a99a896d64cc4e189184b1d
WaleedAls <swyd16@gmail.com>
2025-02-18 20:46:03 +0300

    Waleed Alshammari

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
 fun main() {
-    println("Hello World!")
+    val admin = UserRole.ADMIN
+    val editor = UserRole.EDITOR
+    val viewer= UserRole.VIEWER
+
+    println (haveAccessLevel(admin , 2))
+    println (haveAccessLevel(editor, 2))
+    println (haveAccessLevel(viewer, 2))
+
+
+    for (userRole in UserRole.values()) {
+
+       // println(userRole.accessLevel)
+
+        println ("${userRole.name} - ${userRole.accessLevel}                 ")
+    }
 }
+
+enum class UserRole (var accessLevel: Int){
+    VIEWER(1),  EDITOR(2), ADMIN (3)
+}
+
+fun haveAccessLevel (userRole: UserRole , requireAccess: Int): Boolean {
+    return userRole.accessLevel >= requireAccess
+}
+
+
+
+
